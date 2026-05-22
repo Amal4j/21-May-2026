1628817fd4c28dcf32046e3a60fb386ae2637c95
Amaljeetpractice <amal4j@gmail.com>
2026-05-22 10:41:54 +0530

    a

--- main.tf
+++ main.tf
@@ -9,13 +9,3 @@ variable "rg-location" {
     default = "eastus"
   
 }
-resource "azurerm_resource_group" "pracite-20" {
-name = "20-May-rg"
-location = var.rg-location
-  
-}
-resource "azurerm_resource_group" "pr-20" {
-name = "3xo"
-location = "centralindia"
-  
-}
